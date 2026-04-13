f10e98fdcea5b00203be4f1f052d9e6d38a893d1
Sharad khadka <157219126+shrdk-codes@users.noreply.github.com>
2026-04-13 20:01:13 +0545

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyCJVp6Cx37K-Gua0mdmOf1cvkeg-rn_NDQ",
+    authDomain: "payvora-5723b.firebaseapp.com",
+    projectId: "payvora-5723b",
+    storageBucket: "payvora-5723b.firebasestorage.app",
+    messagingSenderId: "586453742276",
+    appId: "1:586453742276:web:230052700b72c63d59cf91",
+    measurementId: "G-0W72D6E65Q"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
